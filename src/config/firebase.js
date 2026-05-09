072ec9e29942f439f2d2f14d34f28664cfb59f0a
Engels Smith <engelsdamiron9@gmail.com>
2026-05-08 20:50:31 -0400

    Fix messagingPromise export

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getMessaging, isSupported } from 'firebase/messaging';
 
 const firebaseConfig = {
   apiKey: "AIzaSyC-8uUGbnQO9Vvi68fmxn8zKzWJpGehT-g",
@@ -13,6 +14,18 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// Firebase Messaging (solo si el navegador lo soporta)
+export const messagingPromise = isSupported()
+  .then((supported) => {
+    if (supported) {
+      return getMessaging(app);
+    }
+    return null;
+  })
+  .catch(() => null);
+
 export default app;
